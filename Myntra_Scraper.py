933f196852a9d01694a204796e0ada7f57e11220
ad6433 <119733255+ad6433@users.noreply.github.com>
2022-12-03 19:52:05 +0530

    Add files via upload

---
+++ Myntra_Scraper.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import csv
+item = 'shoes'
+prods = []
+
+driver = webdriver.Chrome()
+driver.get('https://www.myntra.com/')
+search_bar = driver.find_element(By.CLASS_NAME, 'desktop-searchBar')
+search_bar.send_keys(item)
+driver.find_element(By.CLASS_NAME, 'desktop-submit').click()
+
+pg = 1
+while True:
+    products = driver.find_elements(By.CLASS_NAME, 'product-base')
+    for prod in products:
+        if 'sneakers' in prod.find_element(By.CLASS_NAME, 'product-product').get_attribute('innerHTML'):
+            prods.append(prod.find_element(By.CLASS_NAME, 'product-brand').get_attribute('innerHTML'), prod.find_element(By.CLASS_NAME, 'product-product').get_attribute('innerHTML'), prod.find_element(By.CLASS_NAME, 'product-ratingsContainer').get_attribute('innerHTML'))
+    if pg == 4:
+        break
+    else:
+        driver.find_element(By.CLASS_NAME, 'pagination-next').click()
+        pg += 1
+
+f = open('MyntraData.csv','w')
+w = csv.writer(f)
+for product_data in prods:
+    w.writerow(product_data)
+f.close()
+        
