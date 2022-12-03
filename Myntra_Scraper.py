ed7c74dfd23ef15b68aa043aa100b2bdbe151ed7
ad6433 <119733255+ad6433@users.noreply.github.com>
2022-12-03 20:16:28 +0530

    Update Myntra_Scraper.py

--- Myntra_Scraper.py
+++ Myntra_Scraper.py
@@ -15,17 +15,17 @@ pg = 1
 while True:
     products = driver.find_elements(By.CLASS_NAME, 'product-base')
     for prod in products:
+        print(prod.find_element(By.CLASS_NAME, 'product-product').get_attribute('innerHTML'))
         if 'Sneakers' in prod.find_element(By.CLASS_NAME, 'product-product').get_attribute('innerHTML'):
-            prods.append(prod.find_element(By.CLASS_NAME, 'product-brand').get_attribute('innerHTML'), prod.find_element(By.CLASS_NAME, 'product-product').get_attribute('innerHTML'), prod.find_element(By.CLASS_NAME, 'product-ratingsContainer').get_attribute('innerHTML'))
+            prods.append([prod.find_element(By.CLASS_NAME, 'product-brand').get_attribute('innerHTML'), prod.find_element(By.CLASS_NAME, 'product-product').get_attribute('innerHTML')])
     if pg == 4:
         break
     else:
         driver.find_element(By.CLASS_NAME, 'pagination-next').click()
         pg += 1
 
-f = open('MyntraData.csv','w')
+f = open('MyntraData.csv','w',newline='')
 w = csv.writer(f)
 for product_data in prods:
     w.writerow(product_data)
 f.close()
-        
